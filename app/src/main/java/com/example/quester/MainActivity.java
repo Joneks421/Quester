86a5035e04b4906af7ef44a5975850e293a7812d
КВАНТОРИАНЕЦ <123@ya.ru>
2021-03-16 16:36:23 +0300

    Still first but edited commit

--- app/src/main/java/com/example/quester/MainActivity.java
+++ app/src/main/java/com/example/quester/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.quester;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_create;
+    Button button_do;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_create = findViewById(R.id.Button_create);
+        button_do = findViewById(R.id.Button_do);
+
     }
 }
